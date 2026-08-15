ca658c837ba0923421860d67b0522a322263abf3
Axeell Cevallos <acevallos31@gmail.com>
2026-08-14 23:14:03 -0600

    feture(Ejercicio2): Declara una variable edad, Muestra un mensaje diferente en pantalla según la edad ingresada (por ejemplo: “Mayor de edad” o “Menor de edad”) utilizando el operador ternario

--- EjerciciosFundamentos/App.js
+++ EjerciciosFundamentos/App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 const nombre = "Axeell";
+const edad  = 39;
 
 
 function saludar(usuario) {
@@ -12,6 +13,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>{saludar(nombre)}</Text>
+      <Text>Edad: {edad >= 18 ? 'Mayor de edad' : 'Menor de edad'}</Text>
       <StatusBar style="auto" />
     </View>
   );
