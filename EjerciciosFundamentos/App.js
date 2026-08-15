fde4e1301631d87004f6154859c8b2f3e2d61dde
Axeell Cevallos <acevallos31@gmail.com>
2026-08-14 23:47:00 -0600

    Feature(Ejercico3): Crea un arreglo de objetos que represente una lista de tareas o productos. Utiliza .map() para renderizar dinámicamente la lista en pantalla como componentes <Text> dentro de un <View>.

--- EjerciciosFundamentos/App.js
+++ EjerciciosFundamentos/App.js
@@ -4,6 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 const nombre = "Axeell";
 const edad  = 39;
 
+const tareas = [
+  { id: 1, nombre: "Estudiar Progra Movil" },
+  { id: 2, nombre: "Realizar la tarea" },
+  { id: 3, nombre: "Subir App.js" },
+  { id: 4, nombre: "Hacer el Examen" },
+  { id: 5, nombre: "Aprobar la CLase" },
+];
 
 function saludar(usuario) {
   return "Hola " + usuario;
@@ -14,6 +21,9 @@ export default function App() {
     <View style={styles.container}>
       <Text>{saludar(nombre)}</Text>
       <Text>Edad: {edad >= 18 ? 'Mayor de edad' : 'Menor de edad'}</Text>
+      {tareas.map((tarea) => (
+        <Text key={tarea.id}>{tarea.nombre}</Text>
+      ))}
       <StatusBar style="auto" />
     </View>
   );
